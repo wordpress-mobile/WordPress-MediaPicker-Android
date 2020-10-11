aaaecc494521227c3e0e2a3545fffab256a71ee7
develric <develric@gmail.com>
2020-10-11 23:28:13 +0200

    Managing different strings in upload dialog depending on insert use case.

--- MediaPicker/src/main/java/org/wordpress/android/mediapicker/insert/MediaInsertUseCase.kt
+++ MediaPicker/src/main/java/org/wordpress/android/mediapicker/insert/MediaInsertUseCase.kt
@@ -1,9 +1,12 @@
 package org.wordpress.android.mediapicker.insert
 
+import org.wordpress.android.R
 import org.wordpress.android.mediapicker.MediaItem.Identifier
 import org.wordpress.android.mediapicker.insert.MediaInsertUseCase.MediaInsertResult.Success
 
 interface MediaInsertUseCase {
+    val actionTitle: Int
+            get() = R.string.media_uploading_default
     suspend fun insert(identifiers: List<Identifier>): MediaInsertResult = Success(identifiers)
 
     sealed class MediaInsertResult {
