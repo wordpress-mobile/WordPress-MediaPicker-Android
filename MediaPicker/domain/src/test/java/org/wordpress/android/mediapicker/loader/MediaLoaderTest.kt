6fbf6925ec3fd1efd222f44b25300c84bba0b9c1
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-07-26 20:00:11 +0200

    Assert dependencies for MediaLoaderTest, make it pass

--- MediaPicker/domain/src/test/java/org/wordpress/android/mediapicker/loader/MediaLoaderTest.kt
+++ MediaPicker/domain/src/test/java/org/wordpress/android/mediapicker/loader/MediaLoaderTest.kt
@@ -3,16 +3,18 @@ package org.wordpress.android.mediapicker.loader
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.isNull
 import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.wordpress.android.BaseUnitTest
-import org.wordpress.android.test
+import org.mockito.junit.MockitoJUnitRunner
 import org.wordpress.android.mediapicker.MediaItem
 import org.wordpress.android.mediapicker.MediaItem.Identifier
 import org.wordpress.android.mediapicker.MediaType.IMAGE
@@ -20,22 +22,19 @@ import org.wordpress.android.mediapicker.MediaType.VIDEO
 import org.wordpress.android.mediapicker.loader.MediaLoader.DomainModel
 import org.wordpress.android.mediapicker.loader.MediaLoader.LoadAction
 import org.wordpress.android.mediapicker.loader.MediaSource.MediaLoadingResult
-import org.wordpress.android.ui.utils.UiString.UiStringText
-import org.wordpress.android.util.NetworkUtilsWrapper
 
-class MediaLoaderTest : BaseUnitTest() {
+@RunWith(MockitoJUnitRunner::class)
+class MediaLoaderTest {
     @Mock lateinit var mediaSource: MediaSource
-    @Mock lateinit var networkUtilsWrapper: NetworkUtilsWrapper
     @Mock lateinit var identifier1: Identifier
     @Mock lateinit var identifier2: Identifier
     private lateinit var mediaLoader: MediaLoader
-    private val mediaTypes = setOf(IMAGE, VIDEO)
     private lateinit var firstMediaItem: MediaItem
     private lateinit var secondMediaItem: MediaItem
 
     @Before
     fun setUp() {
-        mediaLoader = MediaLoader(mediaSource, networkUtilsWrapper)
+        mediaLoader = MediaLoader(mediaSource)
         firstMediaItem = MediaItem(identifier1, "url://first_item", "first item", IMAGE, "image/jpeg", 1)
         secondMediaItem = MediaItem(identifier2, "url://second_item", "second item", VIDEO, "video/mpeg", 2)
     }
@@ -59,9 +58,7 @@ class MediaLoaderTest : BaseUnitTest() {
     fun `shows an error when loading fails`() = withMediaLoader { resultModel, performAction ->
         val errorMessage = "error"
         whenever(mediaSource.load(forced = false, loadMore = false)).thenReturn(
-                MediaLoadingResult.Failure(
-                        UiStringText(errorMessage)
-                )
+            MediaLoadingResult.Failure(errorMessage)
         )
 
         performAction(LoadAction.Start(), true)
@@ -86,21 +83,23 @@ class MediaLoaderTest : BaseUnitTest() {
     }
 
     @Test
-    fun `shows an error when loading next page fails`() = withMediaLoader { resultModel, performAction ->
-        val firstPage = MediaLoadingResult.Success(listOf(firstMediaItem), hasMore = true)
-        val message = "error"
-        val secondPage = MediaLoadingResult.Failure(UiStringText(message), data = listOf(firstMediaItem))
-        whenever(mediaSource.load(forced = false, loadMore = false)).thenReturn(firstPage)
-        whenever(mediaSource.load(forced = false, loadMore = true)).thenReturn(secondPage)
+    fun `shows an error when loading next page fails`() =
+        withMediaLoader { resultModel, performAction ->
+            val firstPage = MediaLoadingResult.Success(listOf(firstMediaItem), hasMore = true)
+            val message = "error"
+            val secondPage =
+                MediaLoadingResult.Failure(message, data = listOf(firstMediaItem))
+            whenever(mediaSource.load(forced = false, loadMore = false)).thenReturn(firstPage)
+            whenever(mediaSource.load(forced = false, loadMore = true)).thenReturn(secondPage)
 
-        performAction(LoadAction.Start(), true)
+            performAction(LoadAction.Start(), true)
 
-        resultModel.assertModel(listOf(firstMediaItem), hasMore = true)
+            resultModel.assertModel(listOf(firstMediaItem), hasMore = true)
 
-        performAction(LoadAction.NextPage, true)
+            performAction(LoadAction.NextPage, true)
 
-        resultModel.assertModel(listOf(firstMediaItem), errorMessage = message, hasMore = true)
-    }
+            resultModel.assertModel(listOf(firstMediaItem), errorMessage = message, hasMore = true)
+        }
 
     @Test
     fun `refresh overrides data`() = withMediaLoader { resultModel, performAction ->
@@ -175,7 +174,7 @@ class MediaLoaderTest : BaseUnitTest() {
         this.last().apply {
             assertThat(this.domainItems).isEqualTo(mediaItems)
             if (errorMessage != null) {
-                assertThat(this.emptyState?.title).isEqualTo(UiStringText(errorMessage))
+                assertThat(this.emptyState?.title).isEqualTo(errorMessage)
                 assertThat(this.emptyState?.isError).isTrue()
             } else {
                 assertThat(this.emptyState?.title).isNull()
@@ -184,6 +183,7 @@ class MediaLoaderTest : BaseUnitTest() {
         }
     }
 
+    @OptIn(InternalCoroutinesApi::class)
     private fun withMediaLoader(
         assertFunction: suspend (
             domainModels: List<DomainModel>,
@@ -193,23 +193,23 @@ class MediaLoaderTest : BaseUnitTest() {
             ) -> Unit
         ) -> Unit
     ) =
-            test {
-                val loadActions: Channel<LoadAction> = Channel()
-                val domainModels: MutableList<DomainModel> = mutableListOf()
-                val job = launch {
-                    mediaLoader.loadMedia(loadActions).collect {
-                        domainModels.add(it)
-                    }
+        runBlocking {
+            val loadActions: Channel<LoadAction> = Channel()
+            val domainModels: MutableList<DomainModel> = mutableListOf()
+            val job = launch {
+                mediaLoader.loadMedia(loadActions).collect {
+                    domainModels.add(it)
                 }
-                assertFunction(domainModels) { action, awaitResult ->
-                    val currentCount = domainModels.size
-                    loadActions.send(action)
-                    if (awaitResult) {
-                        domainModels.awaitResult(currentCount + 1)
-                    }
-                }
-                job.cancel()
             }
+            assertFunction(domainModels) { action, awaitResult ->
+                val currentCount = domainModels.size
+                loadActions.send(action)
+                if (awaitResult) {
+                    domainModels.awaitResult(currentCount + 1)
+                }
+            }
+            job.cancel()
+        }
 
     private suspend fun List<DomainModel>.awaitResult(count: Int) {
         val limit = 10
