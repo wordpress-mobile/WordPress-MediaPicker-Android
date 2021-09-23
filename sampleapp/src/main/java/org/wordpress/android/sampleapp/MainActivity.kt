7997be4632417646bff06d7d800e4551deb00783
Ondrej Ruttkay <ruttkay@gmail.com>
2021-09-23 18:48:01 +0200

    Fix the result handling

--- sampleapp/src/main/java/org/wordpress/android/sampleapp/MainActivity.kt
+++ sampleapp/src/main/java/org/wordpress/android/sampleapp/MainActivity.kt
@@ -3,11 +3,10 @@ package org.wordpress.android.sampleapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import android.widget.Toast.LENGTH_LONG
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import org.wordpress.android.mediapicker.MediaPickerConstants
 import org.wordpress.android.mediapicker.MediaPickerLauncher
 
 @AndroidEntryPoint
@@ -28,10 +27,10 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        val message = data?.extras?.keySet()?.map {
-            data.extras?.get(it)
-        }?.joinToString() ?: ""
+        val message = (data?.extras?.get(MediaPickerConstants.EXTRA_MEDIA_URIS) as? Array<*>)?.map {
+            it as? String
+        }?.joinToString("\n") ?: ""
 
-        Snackbar.make(findViewById<Button>(R.id.content), data.toString(), Snackbar.LENGTH_LONG).show()
+        Snackbar.make(findViewById<Button>(R.id.content), message, Snackbar.LENGTH_LONG).show()
     }
 }
