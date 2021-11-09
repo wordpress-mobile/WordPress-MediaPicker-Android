05f30bda71c62d763846e25ee5595d3138809de3
Ondrej Ruttkay <ruttkay@gmail.com>
2021-11-09 22:26:23 +0100

    Move and remove resources

--- mediapicker/domain/src/main/java/org/wordpress/android/mediapicker/model/MediaType.kt
+++ mediapicker/domain/src/main/java/org/wordpress/android/mediapicker/model/MediaType.kt
@@ -1,5 +1,12 @@
 package org.wordpress.android.mediapicker.model
 
+import org.wordpress.android.mediapicker.model.MediaType.IMAGE
+import org.wordpress.android.mediapicker.model.MediaType.VIDEO
+
 enum class MediaType {
     IMAGE, VIDEO, AUDIO, DOCUMENT
+}
+
+enum class MediaTypes(val allowedTypes: Set<MediaType>) {
+    IMAGES(setOf(IMAGE)), VIDEOS(setOf(VIDEO)), IMAGES_AND_VIDEOS(setOf(IMAGE, VIDEO))
 }
