d5f68966c9a5b1b40a2a35812cd708bd5ff6e326
Ondrej Ruttkay <ruttkay@gmail.com>
2021-11-14 13:58:50 +0100

    Fix the build

--- mediapicker/src/main/java/org/wordpress/android/mediapicker/ui/MediaPickerFragment.kt
+++ mediapicker/src/main/java/org/wordpress/android/mediapicker/ui/MediaPickerFragment.kt
@@ -104,7 +104,7 @@ internal class MediaPickerFragment : Fragment() {
         RequestMultiplePermissions()
     ) { permissions ->
         lifecycleScope.launch {
-            permissionUtils.persistPermissionRequestResults(requireActivity(), permissions, false)
+            permissionUtils.persistPermissionRequestResults(permissions)
             checkStoragePermission()
         }
     }
@@ -114,7 +114,7 @@ internal class MediaPickerFragment : Fragment() {
     ) { permissions ->
         lifecycleScope.launch {
             val allGranted = permissions.values.all { it }
-            permissionUtils.persistPermissionRequestResults(requireActivity(), permissions, true)
+            permissionUtils.persistPermissionRequestResults(permissions)
             if (allGranted) {
                 viewModel.onCameraPermissionsGranted()
             } else {
