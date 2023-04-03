dcd5fc6c9c0e9e3701aaf3c1cbd029d7787a1bd2
Ondrej Ruttkay <ruttkay@gmail.com>
2023-04-04 01:00:08 +0200

    Simplify MediaPickerSetup interface

--- mediapicker/domain/src/main/java/org/wordpress/android/mediapicker/api/MediaPickerSetup.kt
+++ mediapicker/domain/src/main/java/org/wordpress/android/mediapicker/api/MediaPickerSetup.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.StringRes
+import org.wordpress.android.mediapicker.api.MediaPickerSetup.DataSource.CAMERA
+import org.wordpress.android.mediapicker.api.MediaPickerSetup.DataSource.DEVICE
 import org.wordpress.android.mediapicker.model.MediaType
 import org.wordpress.android.mediapicker.model.MediaType.AUDIO
 import org.wordpress.android.mediapicker.model.MediaType.IMAGE
@@ -11,12 +13,11 @@ import org.wordpress.android.mediapicker.model.MediaType.VIDEO
 
 data class MediaPickerSetup(
     val primaryDataSource: DataSource,
-    val availableDataSources: Set<DataSource>,
     val isMultiSelectEnabled: Boolean,
-    val needsAccessToStorage: Boolean,
-    val allowedTypes: Set<MediaType>,
     val areResultsQueued: Boolean,
     val searchMode: SearchMode,
+    val availableDataSources: Set<DataSource> = emptySet(),
+    val allowedTypes: Set<MediaType> = emptySet(),
     @StringRes val title: Int = 0
 ) {
     enum class DataSource {
@@ -27,8 +28,11 @@ data class MediaPickerSetup(
         HIDDEN, VISIBLE_TOGGLED, VISIBLE_UNTOGGLED
     }
 
-    val isStoragePermissionRequired = Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU &&
-        needsAccessToStorage
+    val isReadStoragePermissionRequired = when {
+        primaryDataSource == DEVICE && Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU -> true
+        primaryDataSource == CAMERA && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q -> true
+        else -> false
+    }
 
     val isImagesPermissionRequired = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
             allowedTypes.contains(IMAGE)
@@ -39,12 +43,13 @@ data class MediaPickerSetup(
     val isAudioPermissionRequired = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
             allowedTypes.contains(AUDIO)
 
+    val areMediaPermissionsRequired = isImagesPermissionRequired || isVideoPermissionRequired || isAudioPermissionRequired
+
     fun toBundle(bundle: Bundle) {
         bundle.putInt(KEY_PRIMARY_DATA_SOURCE, primaryDataSource.ordinal)
         bundle.putIntegerArrayList(KEY_AVAILABLE_DATA_SOURCES, ArrayList(availableDataSources.map { it.ordinal }))
         bundle.putIntegerArrayList(KEY_ALLOWED_TYPES, ArrayList(allowedTypes.map { it.ordinal }))
         bundle.putBoolean(KEY_CAN_MULTISELECT, isMultiSelectEnabled)
-        bundle.putBoolean(KEY_REQUIRES_STORAGE_PERMISSIONS, isStoragePermissionRequired)
         bundle.putBoolean(KEY_QUEUE_RESULTS, areResultsQueued)
         bundle.putInt(KEY_SEARCH_MODE, searchMode.ordinal)
         bundle.putInt(KEY_TITLE, title)
@@ -55,7 +60,6 @@ data class MediaPickerSetup(
         intent.putIntegerArrayListExtra(KEY_AVAILABLE_DATA_SOURCES, ArrayList(availableDataSources.map { it.ordinal }))
         intent.putIntegerArrayListExtra(KEY_ALLOWED_TYPES, ArrayList(allowedTypes.map { it.ordinal }))
         intent.putExtra(KEY_CAN_MULTISELECT, isMultiSelectEnabled)
-        intent.putExtra(KEY_REQUIRES_STORAGE_PERMISSIONS, isStoragePermissionRequired)
         intent.putExtra(KEY_QUEUE_RESULTS, areResultsQueued)
         intent.putExtra(KEY_SEARCH_MODE, searchMode.ordinal)
         intent.putExtra(KEY_TITLE, title)
@@ -65,7 +69,6 @@ data class MediaPickerSetup(
         private const val KEY_PRIMARY_DATA_SOURCE = "key_primary_data_source"
         private const val KEY_AVAILABLE_DATA_SOURCES = "key_available_data_sources"
         private const val KEY_CAN_MULTISELECT = "key_can_multiselect"
-        private const val KEY_REQUIRES_STORAGE_PERMISSIONS = "key_requires_storage_permissions"
         private const val KEY_ALLOWED_TYPES = "key_allowed_types"
         private const val KEY_QUEUE_RESULTS = "key_queue_results"
         private const val KEY_SEARCH_MODE = "key_search_mode"
@@ -80,18 +83,16 @@ data class MediaPickerSetup(
                 MediaType.values()[it]
             }.toSet()
             val multipleSelectionAllowed = bundle.getBoolean(KEY_CAN_MULTISELECT)
-            val requiresStoragePermissions = bundle.getBoolean(KEY_REQUIRES_STORAGE_PERMISSIONS)
             val queueResults = bundle.getBoolean(KEY_QUEUE_RESULTS)
             val searchMode = SearchMode.values()[bundle.getInt(KEY_SEARCH_MODE)]
             val title = bundle.getInt(KEY_TITLE)
             return MediaPickerSetup(
                 dataSource,
-                availableDataSources,
                 multipleSelectionAllowed,
-                requiresStoragePermissions,
-                allowedTypes,
                 queueResults,
                 searchMode,
+                availableDataSources,
+                allowedTypes,
                 title
             )
         }
@@ -108,18 +109,16 @@ data class MediaPickerSetup(
                 MediaType.values()[it]
             }.toSet()
             val multipleSelectionAllowed = intent.getBooleanExtra(KEY_CAN_MULTISELECT, false)
-            val requiresStoragePermissions = intent.getBooleanExtra(KEY_REQUIRES_STORAGE_PERMISSIONS, false)
             val queueResults = intent.getBooleanExtra(KEY_QUEUE_RESULTS, false)
             val searchMode = SearchMode.values()[intent.getIntExtra(KEY_SEARCH_MODE, 0)]
             val title = intent.getIntExtra(KEY_TITLE, 0)
             return MediaPickerSetup(
                 dataSource,
-                availableDataSources,
                 multipleSelectionAllowed,
-                requiresStoragePermissions,
-                allowedTypes,
                 queueResults,
                 searchMode,
+                availableDataSources,
+                allowedTypes,
                 title
             )
         }
