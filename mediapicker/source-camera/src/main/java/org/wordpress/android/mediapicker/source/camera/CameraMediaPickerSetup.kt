acf398075510b24d1b38a27e19d92f1b9568b1ea
Ondrej Ruttkay <ruttkay@gmail.com>
2023-11-03 15:12:09 +0100

    Create a new camera module and move relevant classes over

---
+++ mediapicker/source-camera/src/main/java/org/wordpress/android/mediapicker/source/camera/CameraMediaPickerSetup.kt
@@ -0,0 +1,18 @@
+package org.wordpress.android.mediapicker.source.camera
+
+import org.wordpress.android.mediapicker.api.MediaPickerSetup
+import org.wordpress.android.mediapicker.api.MediaPickerSetup.DataSource.CAMERA
+import org.wordpress.android.mediapicker.api.MediaPickerSetup.SearchMode.HIDDEN
+
+class CameraMediaPickerSetup private constructor() {
+    companion object {
+        fun buildCameraPicker(): MediaPickerSetup {
+            return MediaPickerSetup(
+                primaryDataSource = CAMERA,
+                isMultiSelectEnabled = false,
+                areResultsQueued = false,
+                searchMode = HIDDEN
+            )
+        }
+    }
+}
