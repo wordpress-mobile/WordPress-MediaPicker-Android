b6018ce6350a8497fa5724b1ef6ec89901965c1e
Ondrej Ruttkay <ruttkay@gmail.com>
2023-11-06 18:18:43 +0100

    Update glide callbacks

--- mediapicker/src/main/java/org/wordpress/android/mediapicker/ui/MediaViewerFragment.kt
+++ mediapicker/src/main/java/org/wordpress/android/mediapicker/ui/MediaViewerFragment.kt
@@ -14,6 +14,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import dagger.hilt.android.AndroidEntryPoint
 import org.wordpress.android.mediapicker.R
 import org.wordpress.android.mediapicker.databinding.MediaPickerLibViewerFragmentBinding
@@ -105,7 +106,7 @@ internal class MediaViewerFragment :
     override fun onLoadFailed(
         e: GlideException?,
         model: Any?,
-        target: com.bumptech.glide.request.target.Target<Drawable>?,
+        target: Target<Drawable>,
         isFirstResource: Boolean
     ): Boolean {
         binding.progressBar.isVisible = false
@@ -116,10 +117,10 @@ internal class MediaViewerFragment :
      * Glide has loaded the image, hide the progress bar
      */
     override fun onResourceReady(
-        resource: Drawable?,
-        model: Any?,
-        target: com.bumptech.glide.request.target.Target<Drawable>?,
-        dataSource: DataSource?,
+        resource: Drawable,
+        model: Any,
+        target: Target<Drawable>?,
+        dataSource: DataSource,
         isFirstResource: Boolean
     ): Boolean {
         binding.progressBar.isVisible = false
