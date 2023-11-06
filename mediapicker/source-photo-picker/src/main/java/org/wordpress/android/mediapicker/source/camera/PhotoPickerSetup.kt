2054dd8b1779c7c88a46af9afe88ba300e8b86ae
Ondrej Ruttkay <ruttkay@gmail.com>
2023-11-06 16:05:04 +0100

    Add a new photo picker module

---
+++ mediapicker/source-photo-picker/src/main/java/org/wordpress/android/mediapicker/source/camera/PhotoPickerSetup.kt
@@ -0,0 +1,19 @@
+package org.wordpress.android.mediapicker.source.camera
+
+import org.wordpress.android.mediapicker.api.MediaPickerSetup
+import org.wordpress.android.mediapicker.api.MediaPickerSetup.DataSource.CAMERA
+import org.wordpress.android.mediapicker.api.MediaPickerSetup.DataSource.PHOTO_PICKER
+import org.wordpress.android.mediapicker.api.MediaPickerSetup.SearchMode.HIDDEN
+
+class PhotoPickerSetup private constructor() {
+    companion object {
+        fun build(isMultiSelectAllowed: Boolean): MediaPickerSetup {
+            return MediaPickerSetup(
+                primaryDataSource = PHOTO_PICKER,
+                isMultiSelectEnabled = false,
+                areResultsQueued = false,
+                searchMode = HIDDEN
+            )
+        }
+    }
+}
