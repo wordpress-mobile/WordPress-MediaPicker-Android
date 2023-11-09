28b714a3e4a48fe4a97fc767cb2e8062b92116db
Ondrej Ruttkay <ruttkay@gmail.com>
2023-11-09 21:02:23 +0100

    Fix the build

--- sampleapp/src/main/java/org/wordpress/android/sampleapp/MediaPickerSetupFactory.kt
+++ sampleapp/src/main/java/org/wordpress/android/sampleapp/MediaPickerSetupFactory.kt
@@ -7,6 +7,7 @@ import org.wordpress.android.mediapicker.api.MediaPickerSetup.DataSource.DEVICE
 import org.wordpress.android.mediapicker.api.MediaPickerSetup.DataSource.GIF_LIBRARY
 import org.wordpress.android.mediapicker.api.MediaPickerSetup.DataSource.SYSTEM_PICKER
 import org.wordpress.android.mediapicker.model.MediaTypes
+import org.wordpress.android.mediapicker.setup.SystemMediaPickerSetup
 import org.wordpress.android.mediapicker.source.camera.CameraMediaPickerSetup
 import org.wordpress.android.mediapicker.source.device.DeviceMediaPickerSetup
 import org.wordpress.android.mediapicker.source.gif.GifMediaPickerSetup
@@ -18,11 +19,11 @@ class MediaPickerSetupFactory @Inject constructor() : MediaPickerSetup.Factory {
         return when (source) {
             GIF_LIBRARY -> GifMediaPickerSetup.build(canMultiSelect = true)
             CAMERA -> CameraMediaPickerSetup.build()
-            DEVICE -> DeviceMediaPickerSetup.buildMediaPicker(
+            DEVICE -> DeviceMediaPickerSetup.build(
                 mediaTypes = mediaTypes,
                 canMultiSelect = true
             )
-            SYSTEM_PICKER -> DeviceMediaPickerSetup.buildSystemPicker(
+            SYSTEM_PICKER -> SystemMediaPickerSetup.build(
                 mediaTypes = mediaTypes,
                 canMultiSelect = false
             )
